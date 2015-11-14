6baceaf1af86680321fe7886eca8c0133b45a794
Andreas Gwilt <andreas@gwilt.net>
2015-11-14 20:32:28 +0100

    Add Thorin's textures

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 -- Additional clothes for LOTT
 
 dofile(minetest.get_modpath(minetest.get_current_modname()).."/bombur.lua")
+dofile(minetest.get_modpath(minetest.get_current_modname()).."/thorin.lua")
 dofile(minetest.get_modpath(minetest.get_current_modname()).."/dwarf.lua")
---
+++ thorin.lua
@@ -0,0 +1,27 @@
+-- Thorin's outfit
+
+local level=1
+
+-- cap (head)
+minetest.register_tool("moreclothes:cap_thorin", {
+	description = "Thorin's Cap",
+	inventory_image = "moreclothes_inv_cap_thorin.png",
+	groups = {armor_head=0, armor_heal=0, clothes=1},
+	wear = 0
+})
+
+-- shirt (torso)
+minetest.register_tool("moreclothes:shirt_thorin", {
+	description = "Thorin's Shirt",
+	inventory_image = "moreclothes_inv_shirt_thorin.png",
+	groups = {armor_torso=0, armor_heal=0, clothes=1},
+	wear = 0
+})
+
+-- cloak
+minetest.register_tool("moreclothes:cloak_thorin", {
+	description = "Thorin's Cloak",
+	inventory_image = "moreclothes_inv_cloak_thorin.png",
+	groups = {armor_heal=0, clothes=1, no_preview = 1},
+	wear = 0
+})
